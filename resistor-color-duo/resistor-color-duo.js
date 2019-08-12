5d2acba3a43f27ca68d3f070fcbc4b3622008ecd
Corey McCandless <corey.mccandless@atlasied.com>
2019-08-12 10:10:34 -0400

    resistor-color-duo: new solution

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,14 @@
+const bandColor = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const value = bands => bands.reduce((sum, band) => sum * 10 + bandColor[band], 0);
